d37608b16a40f8f5aca28baaea41150508b6e8fc
SergeevDmitry <sergeevdms@gmail.com>
2024-02-05 23:01:09 +0200

    Lowered standard collateral amount to 25k

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,7 +2,7 @@ import { parseEther } from 'ethers/lib/utils'
 
 export const MASTERNODE_ADDRESS = '0x0000000000000000000000000000000000001002'
 
-export const COLLATERAL_AMOUNT = parseEther('1000000')
+export const COLLATERAL_AMOUNT = parseEther('25000')
 export const COLLATERAL_AMOUNT_10K = parseEther('100000')
 export const COLLATERAL_AMOUNT_50K = parseEther('500000')
 export const WITHDRAWAL_DELAY = 100800
