a2d22e26f1aa22dfa29689b784badc8d57c6fdff
igorgoldobin <IgorGoldobin@ssw.com.au>
2024-02-06 12:42:04 +1000

    feat: connect button style

--- src/components/SelectNetwork/index.tsx
+++ src/components/SelectNetwork/index.tsx
@@ -26,7 +26,7 @@ const SelectNetwork = () => {
           </>
         ) : 'Wrong network'}
       </div> */}
-      <a onClick={() => setShowOptions(true)} className="btn-sm text-slate-300 hover:text-white transition duration-150 ease-in-out w-full group [background:linear-gradient(theme(colors.slate.900),_theme(colors.slate.900))_padding-box,_conic-gradient(theme(colors.slate.400),_theme(colors.slate.700)_25%,_theme(colors.slate.700)_75%,_theme(colors.slate.400)_100%)_border-box] relative before:absolute before:inset-0 before:bg-slate-800/30 before:rounded-full before:pointer-events-none">
+      <a onClick={() => setShowOptions(true)} className="btn-sm text-slate-300 bg-purple-400 hover:text-white transition duration-150 ease-in-out w-full group [background:linear-gradient(theme(colors.slate.900),_theme(colors.slate.900))_padding-box,_conic-gradient(theme(colors.slate.400),_theme(colors.slate.700)_25%,_theme(colors.slate.700)_75%,_theme(colors.slate.400)_100%)_border-box] relative before:absolute before:inset-0 before:bg-slate-800/30 before:rounded-full before:pointer-events-none">
         <span className="relative inline-flex items-center flex gap-2">
           {activeNetwork ? (
             <>
