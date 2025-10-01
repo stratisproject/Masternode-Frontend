34c64e8f93e1d999e63e1cee4cc77bb47c951045
SergeevDmitry <sergeevdms@gmail.com>
2025-10-01 12:41:48 +0200

    Change build out directory

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
+  build: {
+    outDir: './build',
+    emptyOutDir: true,
+  },
   resolve: {
     alias: {
       assets: path.resolve(__dirname, './src/assets'),
